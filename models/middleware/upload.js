bbe9656b8528d8da03d2b88296dec3fafc5cbeb6
Kelana Chandra Helyandika <kelanachandra7@gmail.com>
2023-05-19 01:42:46 +0700

    salah tambah data

--- models/middleware/upload.js
+++
@@ -1,9 +0,0 @@
-const util = require('util')
-const Multer = require('multer')
-
-let processFile = Multer({
-    storage: Multer.memoryStorage()
-}).single('file')
-
-let processFileMiddleware = util.promisify(processFile)
-module.exports = processFileMiddleware
